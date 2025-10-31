f98c3d1a9c0268a4ceb49d20f209cdf396c7247c
LuizBuarqueDev <luizgabrielbuarque@gmail.com>
2025-10-31 10:49:18 -0300

    Initialize biometric module as a Spring Boot application
    - Set up Maven project structure with `pom.xml`.
    - Add Spring Boot main application and test classes.
    - Configure Maven Wrapper for project management.
    - Introduce `.gitattributes` for consistent line endings.
    - Add `application.properties` with initial Spring application configuration.

---
+++ src/test/java/br/edu/ifpe/pontoif/biometric/BiometricApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifpe.pontoif.biometric;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiometricApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
